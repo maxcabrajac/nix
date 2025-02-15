f6fea1a74102152ec42f62dad10c37c0ee21520b
maxcabrajac <max@cabrajac.com>
2025-02-15 09:34:20 -0300

    Add zathura

---
+++ progs/zathura.nix
@@ -0,0 +1,25 @@
+{ ... }: {
+	programs.zathura = {
+		enable = true;
+		mappings = {
+			"n" = "scroll down";
+			"N" = "navigate next";
+			"e" = "scroll up";
+			"E" = "navigate previous";
+			"i" = "scroll right";
+			"h" = "scroll left";
+			"u" = "zoom in";
+			"y" = "zoom out";
+			"k" = "search forward";
+			"K" = "search backward";
+		};
+
+		options = {
+			recolor = true;
+			recolor-lightcolor = "#0f1419";
+			recolor-darkcolor = "#ffffff";
+			adjust-open = "width";
+			guioptions = "";
+		};
+	};
+}
