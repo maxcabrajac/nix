3f198badee47327f1c8661da2318f85417bd30ba
Max Cabrajac <max@cabrajac.com>
2025-08-16 19:34:02 -0300

    Add fish completions to nix command

--- modules/hm/fish.nix
+++ modules/hm/fish.nix
@@ -10,51 +10,55 @@
 	;
 
 	cfg = config.programs.fish;
-in [
-	{
-		options.programs.fish.autols = lib.mkEnableOption "autols";
-		config.programs.fish = mkIf cfg.autols {
-			shellInit = /* fish */ ''
-			set AUTOLS_DISABLE false
-			function autols --on-variable PWD
-				if ! ''$AUTOLS_DISABLE
-					ls
+in
+	[
+		{
+			options.programs.fish.autols = lib.mkEnableOption "autols";
+			config.programs.fish = mkIf cfg.autols {
+				shellInit = /* fish */ ''
+				set AUTOLS_DISABLE false
+				function autols --on-variable PWD
+					if ! ''$AUTOLS_DISABLE
+						ls
+					end
 				end
-			end
-			'';
-		};
-	}
-	{
-		options.programs.fish.keepDir = lib.mkEnableOption "keepDir";
-		config.programs.fish = let file = "/tmp/fish_kept_dir"; in mkIf cfg.keepDir {
-			shellAliases.cdd = /* fish */ ''test -f "${file}" && builtin cd (cat ${file})'';
-			interactiveShellInit = /* fish */ ''
-			function keep_dir --on-event fish_prompt
-				pwd > ${file}
-			end
-			cdd
-			'';
-		};
-	}
-	{
-		options.programs.fish = {
-			sessionVariables = mkOption {
-        	  default = { };
-        	  type =
-          	 with types;
-          	 lazyAttrsOf (oneOf [
-            	str
-            	int
-            	path
-          	 ]);
-      	};
-		};
+				'';
+			};
+		}
+		{
+			options.programs.fish.keepDir = lib.mkEnableOption "keepDir";
+			config.programs.fish = let file = "/tmp/fish_kept_dir"; in mkIf cfg.keepDir {
+				shellAliases.cdd = /* fish */ ''test -f "${file}" && builtin cd (cat ${file})'';
+				interactiveShellInit = /* fish */ ''
+				function keep_dir --on-event fish_prompt
+					pwd > ${file}
+				end
+				cdd
+				'';
+			};
+		}
+		{
+			options.programs.fish = {
+				sessionVariables = mkOption {
+		  	  	  default = { };
+		  	  	  type =
+			 	 	 with types;
+			 	 	 lazyAttrsOf (oneOf [
+						str
+						int
+						path
+			 	 	 ]);
+				};
+			};
 
-		config.programs.fish.interactiveShellInit =
-			cfg.sessionVariables
-			|> attrsToList
-			|> map ({ name, value }: ''set -x "${name}" "${builtins.toString value}"'')
-			|> concatLines
-		;
-	}
-] |> (mods: { imports = mods; })
+			config.programs.fish.interactiveShellInit =
+				cfg.sessionVariables
+				|> attrsToList
+				|> map ({ name, value }: ''set -x "${name}" "${builtins.toString value}"'')
+				|> concatLines
+			;
+		}
+		(lib.mkIf cfg.enable {
+			xdg.configFile."fish/completions/nix.fish".source = "${pkgs.nix}/share/fish/vendor_completions.d/nix.fish";
+		})
+	] |> (mods: { imports = mods; })
