701c230d9816aef39e9b13fb4704bf5689a7c08b
Max Cabrajac <max@cabrajac.com>
2025-10-09 01:15:49 -0300

    Add websites

--- users/max/web.nix
+++ users/max/web.nix
@@ -2,7 +2,28 @@
 	web = {
 		browser = pkgs.vivaldi;
 		sites = [
-			{ name = "Hanabi"; alias = "h"; bookmark = "hanab.live"; }
+			{
+				alias = "h";
+				name = "Hanabi";
+				bookmark = "hanab.live";
+			}
+			{
+				alias = "lv";
+				name = "H-group Conventions";
+				bookmark = "hanabi.github.io/learning-path#level-summary";
+				search_engine = "hanabi.github.io/level-%%";
+			}
+			{
+				alias = "ha";
+				name = "Home Assistant";
+				bookmark = "ha.helo.cabrajac.com";
+			}
+			{
+				alias = "y";
+				name = "Home Assistant";
+				bookmark = "youtube.com";
+				search_engine = "youtube.com/results?search_query=%%";
+			}
 		];
 	};
 }
