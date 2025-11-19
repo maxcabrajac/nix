632ccef3e77517d8c938739dd25294b32d3e498d
Max Cabrajac <max@cabrajac.com>
2025-11-18 23:40:54 -0300

    xdg-d-portal config workaround

--- modules/hm/niri/default.nix
+++ modules/hm/niri/default.nix
@@ -4,4 +4,10 @@
 	];
 
 	programs.niri.package = pkgs.niri;
+
+	home.packages = lib.optional config.programs.niri.enable pkgs.nautilus;
+
+	# xdg.portal.config.niri = {
+	# 	"org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+	# };
 }
