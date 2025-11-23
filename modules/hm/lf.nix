87ad7a2259a5f29cb74ccf7b79274a01abe8450f
Max Cabrajac <max@cabrajac.com>
2025-11-22 21:54:58 -0300

    Fix lf references on lfcd

--- modules/hm/lf.nix
+++ modules/hm/lf.nix
@@ -9,9 +9,9 @@ in {
 	};
 
 	config = lib.mkIf cfg.enable {
-		programs.fish.shellAliases.lfcd = "cd (lf -print-last-dir)";
-		programs.bash.shellAliases.lfcd = "cd $(lf -print-last-dir)";
-		programs.zsh.shellAliases.lfcd = "cd $(lf -print-last-dir)";
+		programs.fish.shellAliases.lfcd = "cd (${cfg.package} -print-last-dir)";
+		programs.bash.shellAliases.lfcd = "cd $(${cfg.package} -print-last-dir)";
+		programs.zsh.shellAliases.lfcd = "cd $(${cfg.package} -print-last-dir)";
 
 		xdg.portal.termfilechooser = cfg.useAsXdgPortalOn |> lib.mapAttrs (_: enable: lib.mkIf enable {
 			cmd = "lf-wrapper.sh";
