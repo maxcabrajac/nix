625995b7f1a221e976c922f3cc446a0c2fb5c18e
Max Cabrajac <max@cabrajac.com>
2025-12-24 19:50:43 -0300

    Run nh clean as a service

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,4 +1,23 @@
-{ ... }: {
+{ config, lib, ... }: {
 	nixpkgs.config.allowUnfree = true;
 	nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
+
+	programs.nh = {
+		enable = true;
+		clean = {
+			enable = true;
+			# persistent = true;
+			dates = "Sun *-*-* 12:00";
+			extraArgs = "--keep 10 --keep-since 21d --optimise";
+		};
+	};
+	# Manually set option on the timer
+	systemd.timers.nh-clean.timerConfig.RandomizedDelaySec = "2h";
+	# This should fail if the previous line ever becomes invalid
+	assertions = [
+		{
+			assertion = config.systemd.services |> lib.attrByPath [ "nh-clean" "enable" ] false;
+			message = "nh-clean service doesn't exist. Most likely it was renamed on nixpkgs.";
+		}
+	];
 }
