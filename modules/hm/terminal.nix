7e1f774ae628d1ab142794cfa853b00582803453
Max Cabrajac <max@cabrajac.com>
2025-12-25 07:41:57 -0300

    xdg-terminal-exec

--- modules/hm/terminal.nix
+++ modules/hm/terminal.nix
@@ -3,7 +3,14 @@
 in {
 	options.terminal = {
 		package = lib.mkOption {
-			type = with lib.types; nullOr package;
+			type = with lib.types; package;
+		};
+
+		desktopFile = lib.mkOption {
+			type = lib.types.pathInStore;
+			default = cfg.package
+				|> (p: "${p}/share/applications/${p.pname}.desktop")
+			;
 		};
 
 		bin = lib.mkOption {
@@ -11,8 +18,29 @@ in {
 		};
 	};
 
-	config = {
-		terminal.bin = lib.getExe cfg.package;
-		home.packages = lib.mkIf config.profiles.gui [ cfg.package ];
-	};
+	config = lib.mkMerge [
+		{
+			terminal.bin = lib.getExe cfg.package;
+		}
+		(lib.mkIf config.profiles.gui {
+			home = {
+				packages = [ cfg.package ];
+			};
+
+			assertions = [
+				{
+					assertion = builtins.pathExists cfg.desktopFile;
+					message = "terminal.desktopFile does not exist";
+				}
+			];
+			xdg.terminal-exec = {
+				enable = true;
+				settings = {
+					default = [
+						cfg.desktopFile
+					];
+				};
+			};
+		})
+	];
 }
