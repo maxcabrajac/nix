4bbe4e6b9291fa776320140bdc4515250d320442
Max Cabrajac <max@cabrajac.com>
2026-02-12 00:49:31 -0300

    Enable swap

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -118,7 +118,10 @@
 		};
 	};
 
-	swapDevices = [ ];
+	swapDevices = [ {
+		device = "/var/lib/swapfile";
+		size = 16 * 1024;
+	} ];
 
 	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 	hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
