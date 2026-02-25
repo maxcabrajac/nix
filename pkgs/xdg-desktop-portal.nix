8d420e5286295aebab22177c659c1f5e181e011d
Max Cabrajac <max@cabrajac.com>
2026-02-24 21:08:23 -0300

    Setup an alarm for xdg-portal updates

--- pkgs/xdg-desktop-portal.nix
+++ pkgs/xdg-desktop-portal.nix
@@ -1,7 +1,8 @@
 { inputs, lib, ...  }: {
 	packages = {
 		xdg-desktop-portal = { stdenv }: let
-			xdp-git = inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.xdg-desktop-portal.overrideAttrs (old: {
+			base = inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.xdg-desktop-portal;
+			xdp-git = base.overrideAttrs (old: {
 				src = inputs.xdp-git;
 				doCheck = false;
 				patches = let
@@ -17,8 +18,11 @@
 				in
 					old.patches |> lib.filter (p: ! isRefused p);
 			});
+
+			prLink = "https://github.com/flatpak/xdg-desktop-portal/pull/1867";
+			alarmMessage = "A new xdg-portal release (${base.version}) hit nixpkgs. Time to check if ${prLink} is upstreamed.";
 		in
-			xdp-git;
+			lib.warnIf (base.version != "1.20.3") alarmMessage xdp-git;
 	};
 }
 
