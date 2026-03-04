f1e2e19fb1312a3ed59515775d01c46599825c47
Max Cabrajac <max@cabrajac.com>
2026-03-04 18:36:35 -0300

    ripgrep

---
+++ users/max/ripgrep.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, config, ... }: let
+	cfg = config.programs.ripgrep;
+	bins = pkgs |> lib.mapAttrs (_: p: lib.getExe p);
+in {
+	config = {
+		programs.ripgrep = {
+			enable = true;
+			arguments = lib.flatten [
+				"--hidden"
+				([
+					"**/.git/*"
+					"**/.jj/*"
+					"*.lock"
+				] |> map (x: "--glob=!${x}"))
+			];
+		};
+
+		home.shellAliases = lib.mkIf cfg.enable {
+			ggrep = bins.gnugrep;
+			grep = bins.ripgrep;
+		};
+	};
+}
