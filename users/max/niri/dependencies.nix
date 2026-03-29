464ffead1358e09886e5af27517c9da8aedeaccb
Max Cabrajac <max@cabrajac.com>
2026-03-28 21:01:10 -0300

    Disable wlsunset

--- users/max/niri/dependencies.nix
+++ users/max/niri/dependencies.nix
@@ -6,6 +6,5 @@
  	programs.lf.useAsXdgPortalOn.niri = true;
  	programs.walker.enable = true;
  	services.playerctld.enable = true;
- 	services.wlsunset.enable = true;
 	services.swww.enable = true;
 }
