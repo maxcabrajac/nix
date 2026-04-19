97218803896cca82e24795979f14ae14ece9dd50
Max Cabrajac <max@cabrajac.com>
2026-04-19 16:28:40 -0300

    Add mainMonitor option

--- modules/nixos/monitors.nix
+++ modules/nixos/monitors.nix
@@ -2,43 +2,67 @@
 	t = lib.types;
 	cfg = config.monitors;
 in {
-	options.monitors = lib.mkOption {
-		type = t.attrsOf <| t.submodule {
-			options = let
-				optT = type: lib.mkOption { inherit type; };
-				numOpt = optT t.number;
-			in {
-				x = numOpt;
-				y = numOpt;
-				w = numOpt;
-				h = numOpt;
-				refresh = numOpt;
+	options = {
+		monitors = lib.mkOption {
+			type = t.attrsOf <| t.submodule {
+				options = let
+					optT = type: lib.mkOption { inherit type; };
+					numOpt = optT t.number;
+				in {
+					x = numOpt;
+					y = numOpt;
+					w = numOpt;
+					h = numOpt;
+					refresh = numOpt;
 
-				main = lib.mkOption {
-					type = t.bool;
-					default = false;
+					main = lib.mkOption {
+						type = t.bool;
+						default = false;
+					};
 				};
 			};
-		};
 
-		default = {};
+			default = {};
+ 		};
+
+		mainMonitor = lib.mkOption {
+			type = t.str;
+			readOnly = true;
+		};
 	};
 
-	config = {
-		assertions = [
+	config = let
+		cfgList = cfg |> lib.attrValues;
+		monitorCount = cfgList |> lib.length;
+	in
+		lib.mkMerge [
+			(lib.mkIf (monitorCount != 0) {
+				assertions = [
+					{
+						assertion = let
+							mainMonitorCount = cfgList |> lib.filter (x: x.main) |> lib.length;
+						in mainMonitorCount == 1;
+						message = "Monitor option must have exactly one main monitor.";
+					}
+				];
+
+				mainMonitor = cfg
+					|> lib.filterAttrs (_: m: m.main)
+					|> lib.attrNames
+					|> lib.head
+				;
+			})
 			{
-				assertion = let
-					cfgList = cfg |> lib.attrValues;
-					monitorCount = cfgList |> lib.length;
-					mainMonitorCount = cfgList |> lib.filter (x: x.main) |> lib.length;
-				in (monitorCount != 0) -> (mainMonitorCount == 1);
-				message = "Monitor option must have exactly one main monitor.";
+				hmImport = [
+					{
+						path = [ "monitors" ];
+						value = cfg;
+					}
+					{
+						path = [ "mainMonitor" ];
+						value = config.mainMonitor;
+					}
+				];
 			}
 		];
-
-		hmImport = [{
-			path = [ "monitors" ];
-			value = cfg;
-		}];
-	};
 }
