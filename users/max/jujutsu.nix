1b79019c3f295c4f58d0ab6423fd84d7698280ae
Max Cabrajac <max@cabrajac.com>
2026-04-27 18:26:18 -0300

    Add jj gc

--- users/max/jujutsu.nix
+++ users/max/jujutsu.nix
@@ -8,6 +8,18 @@ in {
 			templates = {
 				git_push_bookmark = ''"maxcabrajac-" ++ change_id.short()'';
 			};
+
+			revset-aliases = let
+				not = x: "(~${x})";
+				ancestors = x: "(::${x})";
+				union = xs: "(${lib.concatStringsSep "|" xs})";
+			in {
+				"unbooked()" = not <| ancestors <| union <| [ "bookmarks()" "remote_bookmarks()" ];
+			};
+
+			aliases = {
+				gc = ["abandon" "-r" "unbooked()"];
+			};
 		};
 	};
 	home.shellAbbrs.nn = lib.mkIf cfg.enable "jj";
