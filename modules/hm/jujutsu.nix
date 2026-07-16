d457339d46d3baa236f7c9f6ffb5e20bc2551837
Max Cabrajac <max@cabrajac.com>
2026-07-15 21:15:00 -0300

    Automatic jujutsu dynamic completions

--- modules/hm/jujutsu.nix
+++ modules/hm/jujutsu.nix
@@ -1,8 +1,16 @@
-{ lib, config, ... }: let
+{ lib, config, pkgs, ... }: let
 	cfg = config.programs.jujutsu;
 	cfgDir = "${config.xdg.configHome}/jj";
-in {
-	home.file = lib.mkIf (cfg.enable && cfg.settings != {})  {
-		"${cfgDir}/config.toml".target = "${cfgDir}/conf.d/hm-managed.toml";
-	};
-}
+in
+	lib.mkIf cfg.enable {
+		home.file = lib.mkIf (cfg.settings != {})  {
+			"${cfgDir}/config.toml".target = "${cfgDir}/conf.d/hm-managed.toml";
+		};
+
+		xdg.configFile.jujutsu-dynamic-fish-completions = {
+			target = "fish/completions/jujutsu.fish";
+			source = pkgs.runCommand "jj-dynamic-fish-completions" { nativeBuildInputs = [ pkgs.jujutsu ]; } ''
+				COMPLETE=fish jj > $out
+			'';
+		};
+	}
