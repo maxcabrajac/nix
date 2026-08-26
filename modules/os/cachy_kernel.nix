58fd4a6c9a77019cd09f87d291b371f2326c111c
Max Cabrajac <max@cabrajac.com>
2026-08-25 21:55:38 -0300

    Drop base-kernel specialization

--- modules/os/cachy_kernel.nix
+++ modules/os/cachy_kernel.nix
@@ -1,12 +1,6 @@
-{ inputs, lib, ... }: {
+{ inputs, ... }: {
 	# TODO: make this platform agnostic
-	os.cachy-kernel-x86 = {pkgs, ... }: {
+	os.cachy-kernel-x86 = {
 		boot.kernelPackages = inputs.cachy-kernel.legacyPackages.x86_64-linux.linuxPackages-cachyos-latest-lto-x86_64-v3;
-		specialisation = {
-			base-kernel.configuration = {
-				system.nixos.tags = [ "base-kernel" ];
-				boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-			};
-		};
 	};
 }
